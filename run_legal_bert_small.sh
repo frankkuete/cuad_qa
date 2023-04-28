6852ac5a653988315aef2ddd46bb020930d02e6d
Frank Jordan KUETE KAMTA <46934865+frankkuete@users.noreply.github.com>
2023-04-28 19:43:55 +0200

    Update run_legal_bert_small.sh

--- run_legal_bert_small.sh
+++ run_legal_bert_small.sh
@@ -6,11 +6,11 @@ python run_qa.py \
   --per_device_train_batch_size 16 \
   --per_device_eval_batch_size 16 \
   --learning_rate 1e-5 \
-  --num_train_epochs 12 \
+  --num_train_epochs 14 \
   --max_seq_length 512 \
   --max_answer_length 512 \
   --doc_stride 256 \
-  --output_dir ../train_models/legal-bert-small-4 \
+  --output_dir ../train_models/legal-bert-small-5 \
   --overwrite_output_dir \
   --save_steps 10000 \
   --version_2_with_negative
