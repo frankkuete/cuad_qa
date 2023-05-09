2a1f6f082c4348b5a817778b1ff8bea39e81e236
frankkuete <fkueteka@hpda.ulb.ac.be>
2023-05-10 00:32:33 +0200

    add results file for electra-small

--- run_elec_small.sh
+++ run_elec_small.sh
@@ -3,14 +3,14 @@ python run_qa_electra.py \
   --dataset_name cuad \
   --do_train \
   --do_predict \
-  --per_device_train_batch_size 32 \
-  --per_device_eval_batch_size 32 \
+  --per_device_train_batch_size 64 \
+  --per_device_eval_batch_size 64 \
   --learning_rate 1e-5 \
-  --num_train_epochs 4 \
+  --num_train_epochs 14 \
   --max_seq_length 512 \
   --max_answer_length 512 \
   --doc_stride 256 \
-  --output_dir ../train_models/electra-small \
+  --output_dir ../train_models/electra-small-6 \
   --overwrite_output_dir \
   --save_steps 10000 \
   --version_2_with_negative
